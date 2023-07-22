3b958a238b22233a79c6ccae9fcea456f3eef8c9
Habeeburahman <48110834+Habeeburahman@users.noreply.github.com>
2023-07-22 19:59:50 +0530

    some changes to view in good aspect

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "874dd286ab19443c5d560c89130f823f",
-"/": "874dd286ab19443c5d560c89130f823f",
-"main.dart.js": "a6a1f47b4a809b9ceec3cd091768390b",
+"index.html": "b9ed4a6569b17628beffb60cad367698",
+"/": "b9ed4a6569b17628beffb60cad367698",
+"main.dart.js": "964887f09717950a5d881f9cd93e1e8d",
 "manifest.json": "42ed8fb2a49bbd16a3a7246e42ba99f7",
 "version.json": "af20325f55e8257438dd6306a0639ab3"
 };
